3c72b054537fea375541140ed7c8ce855b68fff6
Marchend <marchend@MarchenddeMacBook-Pro.local>
2017-05-03 22:58:13 +0800

    Initial Commit

---
+++ ZhiHu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhiHu
+//
+//  Created by Marchend on 2017/5/3.
+//  Copyright © 2017年 Marchend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZhiHu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZhiHu
+//
+//  Created by Marchend on 2017/5/3.
+//  Copyright © 2017年 Marchend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
